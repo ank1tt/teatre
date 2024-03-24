cc39d6f74712a6a0356a046050400dd696473553
Ankit <ank1ttt.work@gmail.com>
2024-03-24 21:22:28 +0530

    Adding the project into my frontend directory

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route , Navigate} from 'react-router-dom';
+import './App.css';
+import LogIn from './components/LogIn';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path = "*" element={<Navigate to = "/login" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
