2165223ccedeed0b53235f442a0b0f833832f87f
Ashwini Kunale <163508141+ashwinikunale@users.noreply.github.com>
2024-04-08 14:32:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ function App() {
           <Route path="/login" element={<LogIn />} />
           <Route path="/register" element={<Register />} />
           <Route path = "*" element={<Navigate to = "/login" />} />
+         <Route path = "/cancelseats" element={<BookingCancellation/>} />
         </Routes>
       </div>
     </Router>
